1dea8a7eb9f0bc91071172a729a1189b186e6940
TingedLicense <90207392+TingedLicense@users.noreply.github.com>
2021-09-29 17:48:42 +0300

    Notes-client

---
+++ Ps122019-server/Ps122019-server/Contexts/NoteContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ps122019_server.Models;
+
+namespace Ps122019_server.Contexts
+{
+    public class NoteContext : DbContext
+    {
+        public NoteContext(DbContextOptions<NoteContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Note> Notes { get; set; }
+    }
+}
---
+++ Ps122019-server/Ps122019-server/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ps122019_server.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Tittle { get; set; }
+        public string Body { get; set; }
+        public DateTime? CreateDate { get; set; }
+    }
+}
