eb65aa48e3f5b075b7f559c7c198d84e50b12770
Diwax1 <andreysmith2018@gmail.com>
2021-09-30 19:29:14 +0300

    Note30serve

--- Ps122019-server/Ps122019-server/Startup.cs
+++ Ps122019-server/Ps122019-server/Startup.cs
@@ -26,7 +26,7 @@ namespace Ps122019_server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<NoteContext>(opt=>opt.UseInMemoryDatabase("Testbase"));
+            services.AddDbContext<NoteContext>(opt=>opt.UseInMemoryDatabase("TestDatabase"));
             services.AddControllers();
         }
 
